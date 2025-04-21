00794abd9a66490116b189126330cb3f74c262fe
Tim Gabrikowski <tim@gabrikowski.de>
2025-04-21 16:25:45 +0200

    hacky dacky bodged converter from sokoban level files to c array of compressed levels

---
+++ Sokoban-to-Bits/README.md
@@ -0,0 +1,15 @@
+# Sokoban to Bits
+
+Convert Sokoban Level TXT file to 3-bit compressed level format
+
+## Cell Types
+
+```
+0. Empty
+1. @ Player
+2. # Wall
+3. . Goal
+4. $ Box
+5. + Player on Goal
+6. * Box on Goal
+```
---
+++ Sokoban-to-Bits/index.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const path = require("path");
+
+let levels = fs.readdirSync(path.join(__dirname, "levels"));
+
+let parsedLevels = [];
+
+for (let l = 0; l < levels.length; l++) {
+  let p = path.join(levels[l]);
+  let level = parseLevel(p);
+  parsedLevels.push(level);
+}
+let s = "{\n\t" + parsedLevels.join(",\n\t") + "\n}";
+fs.writeFileSync(path.join(__dirname, "level.c.txt"), s);
+
+function parseLevel(l) {
+  const level = fs.readFileSync(path.join(__dirname, "levels", l)).toString();
+
+  let lines = level.split("\n").map((line) => line.split(""));
+
+  lines = [...lines, ...Array(10).fill(Array(10).fill(" "))].splice(0, 10);
+
+  lines = lines.map((l) => [...l, ...Array(10).fill(" ")].splice(0, 10));
+
+  let out = "";
+
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      switch (lines[i][j]) {
+        case "@":
+          out += "001";
+          break;
+        case "#":
+          out += "010";
+          break;
+        case ".":
+          out += "011";
+          break;
+        case "$":
+          out += "100";
+          break;
+        case "+":
+          out += "101";
+          break;
+        case "*":
+          out += "110";
+          break;
+        default:
+          out += "000";
+          break;
+      }
+    }
+  }
+
+  let bytes = out.match(/.{1,8}/g).map((byte) => parseInt(byte, 2));
+
+  let hex = bytes.map((byte) => byte.toString(16).padStart(2, "0"));
+
+  return hex.map((b) => "0x" + b).join(", ");
+}
